7d8f674d1d81b2301d7e6154c2cd563268683c6a
CK940628 <a0909977931@gmail.com>
2023-12-11 21:23:58 +0800

    feat(task): 變更待辦事項在頁面載入後後服務取得

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { TaskService } from './service/task.service';
 import { JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -13,9 +13,13 @@ import { Todo } from './model/todo';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   taskService = inject(TaskService);
-  tasks = [new Todo(1, '待辦事項 A'), new Todo(2, '待辦事項 B')];
+  tasks: Todo[] = [];
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getAll();
+  }
 
   onAdd(): void {
     this.taskService.add('待辦事項 C');
