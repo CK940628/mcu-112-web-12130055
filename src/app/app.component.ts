89b5d47c94420f161a41df89a473e46d921f35d7
CK940628 <a0909977931@gmail.com>
2023-12-22 15:28:14 +0800

    feat(todo): 待辦事項查詢作業實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { Observable, startWith, Subject, switchMap } from 'rxjs';
+import {
+  BehaviorSubject,
+  merge,
+  Observable,
+  startWith,
+  Subject,
+  switchMap,
+} from 'rxjs';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { Todo } from './model/todo';
@@ -8,6 +16,7 @@ import { TaskService } from './service/task.service';
 import { TodoDetailComponent } from './todo-detail/todo-detail.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoSearchComponent } from './todo-search/todo-search.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -28,24 +37,33 @@ export class AppComponent implements OnInit {
 
   tasks$!: Observable<Todo[]>;
 
+  readonly search$ = new BehaviorSubject<string | null>(null);
+
   readonly refresh$ = new Subject<void>();
+
   selectedId?: number;
+
   ngOnInit(): void {
-    this.tasks$ = this.refresh$.pipe(
-      startWith(undefined),
-      switchMap(() => this.taskService.getAll())
-    );
+    this.tasks$ = merge(
+      this.refresh$.pipe(startWith(undefined)),
+      this.search$
+    ).pipe(switchMap(() => this.taskService.getAll(this.search$.value)));
   }
 
   onAdd(): void {
     this.taskService.add('待辦事項 C').subscribe(() => this.refresh$.next());
   }
+
   onRemove(id: number): void {
     this.taskService.remove(id).subscribe(() => this.refresh$.next());
   }
+
   onStateChange({ task, state }: { task: Todo; state: boolean }): void {
     this.taskService
       .updateState(task, state)
       .subscribe(() => this.refresh$.next());
+  }
+  onSearch(content: string | null): void {
+    this.search$.next(content);
   }
 }
