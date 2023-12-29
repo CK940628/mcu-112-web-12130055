61bbe4de0d06617c90a937d458c45beb5a33e6e7
CK940628 <a0909977931@gmail.com>
2023-12-29 18:17:36 +0800

    refactor(todo-form): 設定路由繫結至輸入性屬性

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { TaskService } from './services/task.service';
 import { TaskRemoteService } from './services/task-remote.service';
@@ -8,7 +8,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     { provide: TaskService, useClass: TaskRemoteService },
   ],
 };
--- src/app/resolver/todo.resolver.spec.ts
+++ src/app/resolver/todo.resolver.spec.ts
@@ -4,8 +4,8 @@ import { ResolveFn } from '@angular/router';
 import { todoResolver } from './todo.resolver';
 
 describe('todoResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
-      TestBed.runInInjectionContext(() => todoResolver(...resolverParameters));
+  const executeResolver: ResolveFn<boolean> = (...resolverParameters) =>
+    TestBed.runInInjectionContext(() => todoResolver(...resolverParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
